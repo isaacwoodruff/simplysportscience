7719e31617e1e0ac2e435ce651e9a90959b8a638
isaacwoodruff <isaacjahwoodruff@gmail.com>
2019-11-14 14:40:42 +0700

    Added employment type choices in model

--- jobs/models.py
+++ jobs/models.py
@@ -5,14 +5,31 @@ class Job(models.Model):
     """
     A single Job post
     """
+    FULL_TIME = 'Full Time'
+    PART_TIME = 'Part Time'
+    CONTRACT = 'Contract'
+    INTERNSHIP = 'Internship'
+    APPRENTICESHIP = 'Apprenticeship'
+    EMPLOYMENT_TYPE_CHOICES = (
+        (FULL_TIME, 'Full Time'),
+        (PART_TIME, 'Part Time'),
+        (CONTRACT, 'Contract'),
+        (INTERNSHIP, 'Internship'),
+        (APPRENTICESHIP, 'Apprenticeship'),
+    )
+
+    employment_type = models.CharField(max_length=50,
+                                        choices=EMPLOYMENT_TYPE_CHOICES,
+                                        default=FULL_TIME)
     title = models.CharField(max_length=200)
     description = models.TextField()
     location = models.CharField(max_length=100)
     employer = models.CharField(max_length=200)
-    employment_type = models.CharField(max_length=100)
-    created_date = models.DateTimeField(auto_now_add=True)
-    published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
     views = models.IntegerField(default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True,
+                                        null=True,
+                                        default=timezone.now)
 
     def __str__(self):
         return self.title + " - " + self.employer
