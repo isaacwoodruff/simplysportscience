a919483f0a608419fd19658b8560265dcdd474d0
isaacwoodruff <isaacjahwoodruff@gmail.com>
2019-11-14 21:10:38 +0700

    Updated registration forms

--- users/forms.py
+++ users/forms.py
@@ -4,16 +4,20 @@ from django.contrib.auth.forms import UserCreationForm
 
 class EmployerRegistrationForm(UserCreationForm):
     email = forms.EmailField()
-    employer = forms.CharField(max_length=200)
+    company_name = forms.CharField(max_length=200)
+    username = forms.EmailField(required=False)
+    username.widget = username.hidden_widget()
 
     class Meta:
         model = User
-        fields = ["employer", "email", "password1", "password2"]
+        fields = ["company_name", "email", "password1", "password2"]
 
 class CandidateRegistrationForm(UserCreationForm):
     email = forms.EmailField()
     first_name = forms.CharField(max_length=50)
     last_name = forms.CharField(max_length=50)
+    username = forms.EmailField(required=False)
+    username.widget = username.hidden_widget()
 
     class Meta:
         model = User
--- users/views.py
+++ users/views.py
@@ -2,20 +2,25 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import EmployerRegistrationForm, CandidateRegistrationForm
 
-def register(request, title="Register"):
+def register(request, title):
     if request.method == "POST":
         if title == "employers":
             form = EmployerRegistrationForm(request.POST)
-            name = form.cleaned_data.get("employer")
         elif title == "candidates":
             form = CandidateRegistrationForm(request.POST)
-            name = form.cleaned_data.get("first_name")
         if form.is_valid():
-            form.save()
-            messages.success(request, f"Success! Welcome aboard {name}")
+            form_obj = form.save(commit=False)
+            form_obj.username = form.cleaned_data.get("email")
+            form_obj.save()
+            messages.success(request, "Success! Your account is now created.")
     else:
         if title == "employers":
             form = EmployerRegistrationForm()
         elif title == "candidates":
             form = CandidateRegistrationForm()
-    return render(request, "register.html", {"form" : form})
+            
+    context = {
+        "form" : form,
+        "page_title" : title.capitalize(),
+    }
+    return render(request, "register.html", context)
