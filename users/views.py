ded42bce3f7da2d09391fb27965e8371e8043561
isaacwoodruff <isaacjahwoodruff@gmail.com>
2019-11-15 17:12:00 +0700

    Split register into to different user views

--- users/views.py
+++ users/views.py
@@ -1,26 +1,52 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import EmployerRegistrationForm, CandidateRegistrationForm
+from django.contrib.auth.models import User
 
-def register(request, title):
+def register_employer(request):
     if request.method == "POST":
-        if title == "employers":
-            form = EmployerRegistrationForm(request.POST)
-        elif title == "candidates":
-            form = CandidateRegistrationForm(request.POST)
+        form = EmployerRegistrationForm(request.POST)
         if form.is_valid():
             form_obj = form.save(commit=False)
             form_obj.username = form.cleaned_data.get("email")
             form_obj.save()
-            messages.success(request, "Success! Your account is now created.")
+
+            profile = User.objects.filter(username=form_obj.username).first().employerprofile
+            profile.is_employer = True
+            profile.company_name = form.cleaned_data.get("company_name")
+            profile.save()
+
+            messages.success(request, "Success! Your account has been created.")
     else:
-        if title == "employers":
-            form = EmployerRegistrationForm()
-        elif title == "candidates":
-            form = CandidateRegistrationForm()
+        form = EmployerRegistrationForm()
             
     context = {
         "form" : form,
-        "page_title" : title.capitalize(),
+        "page_title" : "For Employers",
+    }
+    return render(request, "register.html", context)
+
+
+def register_candidate(request):
+    if request.method == "POST":
+        form = CandidateRegistrationForm(request.POST)
+        if form.is_valid():
+            form_obj = form.save(commit=False)
+            form_obj.username = form.cleaned_data.get("email")
+            form_obj.save()
+
+            profile = User.objects.filter(username=form_obj.username).first().candidateprofile
+            profile.is_candidate = True
+            profile.first_name = form.cleaned_data.get("first_name")
+            profile.last_name = form.cleaned_data.get("last_name")
+            profile.save()
+
+            messages.success(request, "Success! Your account has been created.")
+    else:
+        form = CandidateRegistrationForm()
+            
+    context = {
+        "form" : form,
+        "page_title" : "For Candidates",
     }
     return render(request, "register.html", context)
