42fef29153f6bd2be841e815c45406c178a62584
isaacwoodruff <isaacjahwoodruff@gmail.com>
2019-11-15 17:12:49 +0700

    Created new employer and candidate models

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import EmployerProfile, CandidateProfile
 
-# Register your models here.
+admin.site.register(EmployerProfile)
+admin.site.register(CandidateProfile)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class EmployerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_employer = models.BooleanField(default=True)
+    company_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.company_name} Profile"
+
+class CandidateProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_candidate = models.BooleanField(default=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} Profile"
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if instance.first_name != "":
+        if created:
+            CandidateProfile.objects.create(user=instance)
+        instance.candidateprofile.save()
+    else:
+        if created:
+            EmployerProfile.objects.create(user=instance)
+        instance.employerprofile.save()
+    
