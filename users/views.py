e6f20f560503a1bbb2ecac7b7c346334b5c41ce3
isaacwoodruff <isaacjahwoodruff@gmail.com>
2019-11-16 16:28:11 +0700

    Created user profile views

--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import EmployerRegistrationForm, CandidateRegistrationForm
+from django.contrib.auth.decorators import login_required
+from .forms import EmployerRegistrationForm, CandidateRegistrationForm, CandidateUpdateForm, EmployerUpdateForm, EmployerProfileUpdateForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+
 
 def register_employer(request):
     if request.method == "POST":
@@ -11,18 +14,20 @@ def register_employer(request):
             form_obj.username = form.cleaned_data.get("email")
             form_obj.save()
 
-            profile = User.objects.filter(username=form_obj.username).first().employerprofile
+            profile = User.objects.filter(
+                username=form_obj.username).first().employerprofile
             profile.is_employer = True
             profile.company_name = form.cleaned_data.get("company_name")
             profile.save()
 
-            messages.success(request, "Success! Your account has been created.")
+            messages.success(
+                request, "Success! Your account has been created.")
     else:
         form = EmployerRegistrationForm()
-            
+
     context = {
-        "form" : form,
-        "page_title" : "For Employers",
+        "form": form,
+        "page_title": "For Employers",
     }
     return render(request, "register.html", context)
 
@@ -35,18 +40,54 @@ def register_candidate(request):
             form_obj.username = form.cleaned_data.get("email")
             form_obj.save()
 
-            profile = User.objects.filter(username=form_obj.username).first().candidateprofile
+            profile = User.objects.filter(
+                username=form_obj.username).first().candidateprofile
             profile.is_candidate = True
             profile.first_name = form.cleaned_data.get("first_name")
             profile.last_name = form.cleaned_data.get("last_name")
             profile.save()
 
-            messages.success(request, "Success! Your account has been created.")
+            messages.success(
+                request, "Success! Your account has been created.")
     else:
         form = CandidateRegistrationForm()
-            
+
     context = {
-        "form" : form,
-        "page_title" : "For Candidates",
+        "form": form,
+        "page_title": "For Candidates",
     }
     return render(request, "register.html", context)
+
+
+def logged_user_type(request):
+    try:
+        request.user.employerprofile
+    except ObjectDoesNotExist:
+        user_type = 'candidate_profile'
+    else:
+        user_type = 'employer_profile'
+
+    return redirect(user_type)
+
+
+@login_required
+def employer_profile(request):
+    update_form = EmployerUpdateForm()
+    profile_form = EmployerProfileUpdateForm()
+
+    context = {
+        "update_form": update_form,
+        "profile_form": profile_form,
+    }
+
+    return render(request, "profile.html", context)
+
+@login_required
+def candidate_profile(request):
+    update_form = CandidateUpdateForm()
+
+    context = {
+        "update_form": update_form,
+    }
+
+    return render(request, "profile.html", context)
