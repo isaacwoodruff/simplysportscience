f9000ee93662d27c7e2598df96f7372259fa73d4
isaacwoodruff <isaacjahwoodruff@gmail.com>
2019-11-16 17:28:54 +0700

    Added register links and view, updated urls

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from .views import register_employer, register_candidate, employer_profile, candidate_profile, logged_user_type
+from users import views as user_views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('register/employer', register_employer, name="register_employer"),
-    path('register/candidate', register_candidate, name="register_candidate"),
+    path('register', user_views.register_choice, name="register"),
+    path('register/employer', user_views.register_employer, name="register_employer"),
+    path('register/candidate', user_views.register_candidate, name="register_candidate"),
     path('login', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     path('logout', auth_views.LogoutView.as_view(template_name="logout.html"), name="logout"),
-    path('profile-redirect', logged_user_type, name="logged_user_type"),
-    path('employer-profile', employer_profile, name="employer_profile"),
-    path('candidate-profile', candidate_profile, name="candidate_profile"),
+    path('profile-redirect', user_views.logged_user_type, name="logged_user_type"),
+    path('employer-profile', user_views.employer_profile, name="employer_profile"),
+    path('candidate-profile', user_views.candidate_profile, name="candidate_profile"),
 ]
