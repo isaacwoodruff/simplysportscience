f9f0d1f7e200cf494a0f3d4d27ca8e2a92c09840
isaacwoodruff <isaacjahwoodruff@gmail.com>
2019-11-17 21:47:54 +0700

    Added new list display for both types of users

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import EmployerProfile, CandidateProfile
 
-admin.site.register(EmployerProfile)
-admin.site.register(CandidateProfile)
+class EmployerAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'company_name', 'is_employer', 'is_candidate', 'user',)
+
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'first_name', 'last_name', 'is_employer', 'is_candidate', 'user',)
+
+admin.site.register(EmployerProfile, EmployerAdmin)
+admin.site.register(CandidateProfile, CandidateAdmin)
