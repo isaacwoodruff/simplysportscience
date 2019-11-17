b243e7eeab0b32c89c58e59b8d64c6884df03d48
isaacwoodruff <isaacjahwoodruff@gmail.com>
2019-11-17 21:54:12 +0700

    Added new list display for job posts

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Job
 
-admin.site.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'employer', 'title', 'location', 'employment_type', 'created_date', 'published_date',)
+
+admin.site.register(Job, JobAdmin)
