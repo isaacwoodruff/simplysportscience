5e0a7c56d35635c0914c71d84715eb5c8dff1510
isaacwoodruff <isaacjahwoodruff@gmail.com>
2019-11-20 18:25:53 +0700

    Added search and search autocomplete for job title

--- search/urls.py
+++ search/urls.py
@@ -3,6 +3,8 @@ from search import views as search_views
 
 urlpatterns = [
     path('', search_views.job_list, name="jobs"),
+    path('ajax-search/', search_views.autocomplete_title_search, name="autocomplete_title_search"),
     path('<pk>/', search_views.employer_job_list, name="employer_job_list"),
     path('<slug:slug>/<pk>/', search_views.employer_job_list, name="employer_job_list"),
+    path('results', search_views.search_results, name="search_results"),
 ]
--- search/views.py
+++ search/views.py
@@ -1,23 +1,69 @@
 import os
-from django.shortcuts import render, get_object_or_404
+import json
+from django.shortcuts import render, get_object_or_404, HttpResponse
+from django.db.models import Q
 from jobs.models import Job, EmployerProfile
 
 def job_list(request):
+    algolia_key = os.environ.get('ALGOLIA_PUBLIC_KEY')
+    algolia_app_id = os.environ.get('ALGOLIA_PUBLIC_APP_ID')
+
     context = {
         "page_title": "Jobs",
         "posts": Job.objects.all(),
-        "ALGOLIA_PUBLIC_KEY": os.environ.get('ALGOLIA_PUBLIC_KEY'),
-        "ALGOLIA_PUBLIC_APP_ID": os.environ.get('ALGOLIA_PUBLIC_APP_ID'),
+        "ALGOLIA_PUBLIC_KEY": algolia_key,
+        "ALGOLIA_PUBLIC_APP_ID": algolia_app_id,
     }
     return render(request, "job-list.html", context)
 
 def employer_job_list(request, pk, slug=""):
     employer_object = get_object_or_404(EmployerProfile, pk=pk)
 
+    page_title = employer_object.company_name + " " + "Jobs"
+    posts = Job.objects.filter(employer_fk=employer_object)
+    algolia_key = os.environ.get('ALGOLIA_PUBLIC_KEY')
+    algolia_app_id = os.environ.get('ALGOLIA_PUBLIC_APP_ID')
+
     context = {
-        "page_title": employer_object.company_name + " " + "Jobs",
-        "posts": Job.objects.filter(employer_fk=employer_object),
-        "ALGOLIA_PUBLIC_KEY": os.environ.get('ALGOLIA_PUBLIC_KEY'),
-        "ALGOLIA_PUBLIC_APP_ID": os.environ.get('ALGOLIA_PUBLIC_APP_ID'),
+        "page_title": page_title,
+        "posts": posts,
+        "ALGOLIA_PUBLIC_KEY": algolia_key,
+        "ALGOLIA_PUBLIC_APP_ID": algolia_app_id,
+    }
+    return render(request, "job-list.html", context)
+
+def autocomplete_title_search(request):
+    if request.is_ajax():
+        job_query = request.GET.get('term', '')
+        titles = Job.objects.filter(
+            Q(title__istartswith=job_query) | Q(title__icontains=job_query)
+        )
+        result = []
+        for title in titles:
+            title_json = title.title
+            result.append(title_json)
+        data = json.dumps(result)
+        mimetype = 'application/json'
+    return HttpResponse(data, mimetype)
+
+def search_results(request):
+    job_query = request.GET.get('job-title')
+    location_query = request.GET.get('location')
+    posts = Job.objects.filter(
+        title__icontains=job_query, location__icontains=location_query
+    )
+
+    if location_query != "":
+        page_title = job_query.title() + " Jobs in " + location_query.title()
+    else:
+        page_title = job_query.title() + " Jobs Everywhere"
+
+    algolia_key = os.environ.get('ALGOLIA_PUBLIC_KEY')
+    algolia_app_id = os.environ.get('ALGOLIA_PUBLIC_APP_ID')
+    context = {
+        "page_title": page_title,
+        "posts": posts,
+        "ALGOLIA_PUBLIC_KEY": algolia_key,
+        "ALGOLIA_PUBLIC_APP_ID": algolia_app_id,
     }
     return render(request, "job-list.html", context)
