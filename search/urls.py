57600a0429038380ede140e3ab848644a65ea100
isaacwoodruff <isaacjahwoodruff@gmail.com>
2019-11-25 22:15:20 +0700

    Added slash to end of urls

--- search/urls.py
+++ search/urls.py
@@ -3,10 +3,10 @@ from search import views as search_views
 
 urlpatterns = [
     path('', search_views.job_list, name="jobs"),
+    path('results/', search_views.search_results, name="search_results"),
     path('ajax-search/', search_views.autocomplete_title_search, name="autocomplete_title_search"),
     path('<pk>/', search_views.employer_job_list, name="employer_job_list"),
     path('<slug:slug>/<pk>/', search_views.employer_job_list, name="employer_job_list"),
-    path('results', search_views.search_results, name="search_results"),
-    path('employment', search_views.employment_type_job_list, name="employment_type_job_list"),
-    path('location', search_views.location_job_list, name="location_job_list"),
+    path('employment/', search_views.employment_type_job_list, name="employment_type_job_list"),
+    path('location/', search_views.location_job_list, name="location_job_list"),
 ]
