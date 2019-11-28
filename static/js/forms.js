7b01c97f78e2502d4e159562a2359e2e4e79d219
isaacwoodruff <isaacjahwoodruff@gmail.com>
2019-11-28 09:21:02 +0700

    Added autoFocus

--- static/js/forms.js
+++ static/js/forms.js
@@ -2,11 +2,13 @@ $(document).ready(function () {
     /*
     Starts the autocomplete method with the database titles the view provides.
     It starts from when a user types 1 character with a delay of 100ms.
+    autoFocus is used to automatically select the first result from the list.
     */
     $("#job-title").autocomplete({
         source: autocomplete_title_source,
         minLength: 1,
         delay: 100,
+        autoFocus: true,
     });
 
     /*
