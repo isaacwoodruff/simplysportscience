2cdaaf89c3b9b1c828c53ecaf10b84acc902cf9c
isaacwoodruff <isaacjahwoodruff@gmail.com>
2019-11-28 21:03:26 +0700

    Updated webhook with checkout.session.completed

--- checkout/views.py
+++ checkout/views.py
@@ -60,15 +60,9 @@ def webhook_view(request):
         return HttpResponse(status=400)
 
     # Handle the event
-    if event.type == 'payment_intent.succeeded':
-        payment_intent = event.data.object  # contains a stripe.PaymentIntent
-        print('PaymentIntent was successful!')
-    elif event.type == 'payment_method.attached':
-        payment_method = event.data.object  # contains a stripe.PaymentMethod
-        print('PaymentMethod was attached to a Customer!')
-    # ... handle other event types
+    if event.type == 'checkout.session.completed':
+        print('Checkout session was successful!')
     else:
         # Unexpected event type
         return HttpResponse(status=400)
-
     return HttpResponse(status=200)
