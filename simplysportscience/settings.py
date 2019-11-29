641b321d831f245d2d1aba1aa61b60378692096b
isaacwoodruff <isaacjahwoodruff@gmail.com>
2019-11-29 18:41:27 +0700

    Added password reset by email

--- simplysportscience/settings.py
+++ simplysportscience/settings.py
@@ -146,3 +146,12 @@ LOGIN_URL = "login"
 STRIPE_PUBLISHABLE = os.environ.get("STRIPE_PUBLISHABLE")
 STRIPE_SECRET = os.environ.get("STRIPE_SECRET")
 ENDPOINT_SECRET = os.environ.get("ENDPOINT_SECRET")
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- users/urls.py
+++ users/urls.py
@@ -5,12 +5,26 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('employers/', user_views.employers_page, name="employers"),
     path('candidates/', user_views.candidates_page, name="candidates"),
-    path('register/employer/', user_views.register_employer, name="register_employer"),
-    path('register/candidate/', user_views.register_candidate, name="register_candidate"),
+    path('register/employer/', user_views.register_employer,
+         name="register_employer"),
+    path('register/candidate/', user_views.register_candidate,
+         name="register_candidate"),
     path('login/', user_views.login_view, name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name="logout.html"), name="logout"),
     path('profile-redirect/', user_views.logged_user_type, name="logged_user_type"),
     path('employer-profile/', user_views.employer_profile, name="employer_profile"),
-    path('candidate-profile/', user_views.candidate_profile, name="candidate_profile"),
+    path('candidate-profile/', user_views.candidate_profile,
+         name="candidate_profile"),
     path('delete-account/', user_views.delete_user_view, name="delete_user_view"),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'),
+         name='password_reset'),
+    path('password-reset-done/', auth_views.PasswordResetDoneView.as_view(
+         template_name='password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+         template_name='password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+         template_name='password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
