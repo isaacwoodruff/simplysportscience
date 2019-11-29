83acf007dc88f74cdbde21333c8f084bfdd4e63d
isaacwoodruff <isaacjahwoodruff@gmail.com>
2019-11-29 13:48:07 +0700

    Added AJAX auto update for credit amount

--- static/js/main.js
+++ static/js/main.js
@@ -3,4 +3,19 @@ $(document).ready(function () {
     $("#back-btn").click(function () {
         window.history.back();
     });
+
+    function request_credit_score() {
+
+        req = $.ajax({
+            url: '/update',
+        });
+
+        req.done(function (data) {
+            $('#credit-num').text(data.credit_amount);
+        });
+
+        setTimeout(request_credit_score(), 5000);
+    };
+
+    request_credit_score();
 });
