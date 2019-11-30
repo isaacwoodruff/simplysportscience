ece1c6b5a218adce7ab8e95aa9983e1316c538af
isaacwoodruff <isaacjahwoodruff@gmail.com>
2019-11-30 16:46:33 +0700

    Set DEBUG default

--- simplysportscience/settings.py
+++ simplysportscience/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = [os.environ.get('HOSTNAME')]
 
